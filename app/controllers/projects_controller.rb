652eeaae0941c75fef6e23ac7b9b897f1ad791f0
shereenmv@gmail.com <shereenmv@gmail.com>
2021-12-07 18:02:51 +0800

    controller for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,19 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to dashboard_path
+  end
+
 end
