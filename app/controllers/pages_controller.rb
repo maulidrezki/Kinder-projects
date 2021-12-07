2725ef8a2626d0c230b9dbeba1d8c37c5a96e4e6
Yeon Kim <awyeon@gmail.com>
2021-12-07 17:53:04 +0800

    method home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,16 @@
 class PagesController < ApplicationController
-  def home
-    # if user_signed_in?
-    #   redirect_to root_path
-    # end
 
-  #   if params[:query].present?
-  #     @query = params[:query]
-  #     @projects = Projectt.where("title iLike :query OR location iLike :query", query: "%#{params[:query]}%")
-  #   else
-  #     @projects = Project.all.first(5)
-  #   end
+  def home
+    if user_signed_in?
+      redirect_to root_path
+    end
+
+    if params[:query].present?
+      @query = params[:query]
+      @projects = Projectt.where("title iLike :query OR location iLike :query", query: "%#{params[:query]}%")
+    else
+      @projects = Project.all.first(5)
+    end
   end
 
   def dashboard
