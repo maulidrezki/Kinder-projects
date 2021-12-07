4fd2f25dee71f8745cf2db3242baa99db16f55c4
Yeon Kim <awyeon@gmail.com>
2021-12-07 17:31:49 +0800

    pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,15 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
-
   def home
+
+    # if user_signed_in?
+    #   redirect_to root_path
+    # end
+
+  #   if params[:query].present?
+  #     @query = params[:query]
+  #     @projects = Projectt.where("title iLike :query OR location iLike :query", query: "%#{params[:query]}%")
+  #   else
+  #     @projects = Project.all.first(5)
+  #   end
   end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,44 @@
 class ProjectsController < ApplicationController
+  def index
+    if params[:query].present?
+      @query = params[:query]
+      @projects = @projects.where("title iLike :query OR location iLike :query", query: "%#{params[:query]}%")
+    else
+      @projects = @projects.all
+    end
+
+    if params[:title].present?
+      @query = params[:title]
+      @projects = @projects.where("title >= :query", query: @query)
+    end
+
+    if params[:location].present?
+      @query = params[:location]
+      @projects = @projects.where("location <= :query", query: @query)
+    end
+  end
+
+  def show
+    @project = Project.find(params[:id])
+    @volunteering = Volunteering.new
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title,
+                                    :location,
+                                    :description,
+                                    :contact,
+                                    :status,
+                                    :instagram_link,
+                                    :facebook_link,
+                                    :website_link,
+                                    :linkedin_link,
+                                    :start_date,
+                                    :end_date,
+                                    :start_tim,
+                                    :end_time,
+                                    photos: [])
+  end
 end
