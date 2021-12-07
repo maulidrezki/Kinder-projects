6206a4c33e09914b15919ab6c4567f0349d656ee
Maulid Rezki <maulidrezki13@gmail.com>
2021-12-07 16:51:49 +0700

    volunteering controller pages controller and both views and routes done

--- app/controllers/volunteerings_controller.rb
+++ app/controllers/volunteerings_controller.rb
@@ -1,2 +1,10 @@
 class VolunteeringsController < ApplicationController
+  def edit
+    @volunteering = Volunteering.find(params[:id])
+  end
+
+  def new
+    @volunteering = Volunteering.new
+  end
+
 end
