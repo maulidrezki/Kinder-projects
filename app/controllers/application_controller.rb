228ce5853e038d02cb9ed0b1bda6494d42bc5ad3
Claudia Manchon <claudiambomb@gmail.com>
2021-12-08 18:41:20 +0800

    fix seed and display name field

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :avatar, :about_me])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :display_name, :avatar, :about_me])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :avatar, :about_me])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :display_name, :avatar, :about_me])
   end
 
   def destroy_user_session_path
