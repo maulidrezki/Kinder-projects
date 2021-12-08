8af48d9b52fb009754e2d350fe02726cf479eb91
Claudia Manchon <claudiambomb@gmail.com>
2021-12-08 10:23:27 +0800

    link to volunteer

--- app/controllers/volunteerings_controller.rb
+++ app/controllers/volunteerings_controller.rb
@@ -9,13 +9,11 @@ class VolunteeringsController < ApplicationController
     @volunteering.user = current_user
     @volunteering.project = @project
     @volunteering.status = "pending"
-
-    if @volunteering.save
-      redirect_to dashboard_path
-    else
-      redirect_to new_usser_session_path
-    end
+    @volunteering.save!
+    # if @volunteering.save
+    #   redirect_to dashboard_path
+    # else
+    #   redirect_to new_user_session_path
+    # end
   end
-
-
 end
