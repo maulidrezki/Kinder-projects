18dc538488d884ae2880e3e36cb2d15df579af30
Maulid Rezki <maulidrezki13@gmail.com>
2021-12-09 17:01:58 +0700

    added ngrok config and remove hyperlink text on dashboard

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,6 +10,10 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  #ngrok config
+  
+  config.hosts << /.*ngrok.io/
+
   # Do not eager load code on boot.
   config.eager_load = false
 
