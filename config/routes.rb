8dc7264194ace7b8abe7781c680b70a7cde38d10
Yeon Kim <awyeon@gmail.com>
2021-12-09 16:27:42 +0800

    search bar works

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  resources :projects, only: :index
   resources :projects do
     resources :volunteerings, only: [:create]
   end
