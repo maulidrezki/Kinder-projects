fa204ff9f70b91f61e4d281ac9cfb96faea22074
Yeon Kim <awyeon@gmail.com>
2021-12-13 10:12:24 +0800

    installed gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
+  # acts_as_favoritable
+
   has_many :projects
   has_many :volunteerings
   has_many :messages
