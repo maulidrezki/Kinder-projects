327ae031ce0541b9604438ecc113219deae0ff7b
Yeon Kim <awyeon@gmail.com>
2021-12-14 17:37:50 +0800

    search js mvmt fixed

--- app/javascript/packs/scroll_search.js
+++ app/javascript/packs/scroll_search.js
@@ -1,9 +1,19 @@
 const scrollSearch = () => {
   document.addEventListener("scroll", e => {
-    if (window.scrollY > document.querySelector(".banner").offsetHeight) {
-      document.querySelector(".search-container").classList.add("search-container-back")
-    } else {
-      document.querySelector(".search-container").classList.remove("search-container-back")
+    const searchContainer = document.querySelector(".search-container");
+    const banner = document.querySelector(".banner");
+    // console.log(window.scrollY)
+    // console.log(banner.offsetHeight)
+    if (searchContainer && banner) {
+      if ((window.scrollY + 160) > banner.offsetHeight) {
+        searchContainer.style.top = 0;
+        searchContainer.classList.add("search-container-back")
+      } else {
+        if (window.scrollY === 0) {
+          searchContainer.style.top = "160px";
+        }
+        searchContainer.classList.remove("search-container-back")
+      }
     }
   })
 
