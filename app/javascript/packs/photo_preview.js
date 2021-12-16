498385443c2cf04fd67f798cce3a2e0cc98b7bd4
Maulid Rezki <maulidrezki13@gmail.com>
2021-12-16 10:52:49 +0700

    fixed chatroom image preview

--- app/javascript/packs/photo_preview.js
+++ app/javascript/packs/photo_preview.js
@@ -1,7 +1,8 @@
 // app/javascript/components/photo_preview.js
 const previewImageOnFileSelect = () => {
   // we select the photo input
-  const input = document.getElementById('photo-input');
+  const input = document.getElementById('message_photo');
+  // console.log(input)
   if (input) {
     // we add a listener to know when a new picture is uploaded
     input.addEventListener('change', () => {
@@ -12,13 +13,15 @@ const previewImageOnFileSelect = () => {
 }
 
 const displayPreview = (input) => {
+  // console.log(input)
   if (input.files && input.files[0]) {
     const reader = new FileReader();
     reader.onload = (event) => {
+      // console.log(event.currentTarget.result)
       document.getElementById('photo-preview').src = event.currentTarget.result;
     }
     reader.readAsDataURL(input.files[0])
-    document.getElementById('photo-preview').classList.remove('hidden');
+    document.getElementById('photo-preview').classList.remove('d-none');
   }
 }
 
